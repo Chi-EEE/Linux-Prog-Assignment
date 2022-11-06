a428c9ef8d8717c5a05aeb24f102ee666cfbbb2e
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2022-11-06 19:11:37 +0000

    Moved statements around for correctness

--- ProcessCreation.cpp
+++ ProcessCreation.cpp
@@ -17,15 +17,15 @@ int main()
    int currentPID = getpid();
    int parentPID = getppid();
    std::string message = "Process PID: ";
-   if (pid == 0) // This will be the Parent Process that can create Child Processes
-   {
-      message = "[P0] This is a Parent Process.\n[P0] Parent " + message;
-      message += std::to_string(pid) + "\n" + "[P0] Parent PID: " + std::to_string(parentPID) + "\n" + "[P0] Current PID: " + std::to_string(currentPID) + "\n";
-   }
-   else if (pid > 0) // This will be the Child Process that cannot create Child Processes
+   if (pid == 0) // This will be the Child Process that cannot create Child Processes
    {
       message = "[C] This is a Child Process.\n[C] Child " + message;
       message += std::to_string(pid) + "\n" + "[C] Parent PID: " + std::to_string(parentPID) + "\n" + "[C] Current PID: " + std::to_string(currentPID) + "\n";
+   }
+   else if (pid > 0) // This will be the Parent Process that can create Child Processes
+   {
+      message = "[P0] This is a Parent Process.\n[P0] Parent " + message;
+      message += std::to_string(pid) + "\n" + "[P0] Parent PID: " + std::to_string(parentPID) + "\n" + "[P0] Current PID: " + std::to_string(currentPID) + "\n";
    }
    else if (pid == -1) // This will be the Parent Process that cannot create Child Processes
    {
