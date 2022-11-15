43a89d77bba02f7680ede20910d1fecb45451d1f
Chi Huu Huynh <73843190+Chi+EEE@users.noreply.github.com>
2022-11-15 09:07:07 +0000

    Added cout to the functions

--- ThreadSynchronisation.cpp
+++ ThreadSynchronisation.cpp
@@ -10,25 +10,28 @@
 #include <cstdint>
 #include <atomic>
 
-// Sleep Function
+// Sleep Function: https://stackoverflow.com/questions/4184468/sleep-for-milliseconds
 #include <chrono>
 #include <thread>
 
 std::atomic<int> number{3};
 void *addition(void *_arg)
 {
-    int saved_number = number;
+    int cpuRegister1 = number;
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    
-    number.fetch_add((long)_arg);
+    cpuRegister1 += (long)_arg;
+    number = cpuRegister1;
+    std::cout << "Added: " << number << "\n";
 
     char *ret;
     pthread_exit(ret);
 }
 void *subtraction(void *_arg)
 {
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    number.fetch_add((long)_arg);
+    int cpuRegister1 = number;
+    cpuRegister1 -= (long)_arg;
+    number = cpuRegister1;
+    std::cout << "Subtracted: " << number << "\n";
 
     char *ret;
     pthread_exit(ret);
@@ -39,9 +42,6 @@ int main()
     pthread_t thread_1, thread_2;
     pthread_create(&thread_1, NULL, addition, (void *)2);
     pthread_create(&thread_2, NULL, subtraction, (void *)-2);
-    while (true)
-    {
-        std::cout << number << "\n";
-    }
+    std::cout << "Default: " << number << "\n";
     return 1;
 }
