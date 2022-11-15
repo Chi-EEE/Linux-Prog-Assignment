4c72436c33fb9d9e08ce618dc3d39c2b374fe754
Chi Huu Huynh <73843190+Chi+EEE@users.noreply.github.com>
2022-11-15 14:54:51 +0000

    Made ThreadSynchronisationWithoutLock

--- ThreadSynchronisation.cpp
+++ ThreadSynchronisation.cpp
@@ -10,18 +10,18 @@
 #include <cstdint>
 #include <atomic>
 
-// Sleep Function: https://stackoverflow.com/questions/4184468/sleep-for-milliseconds
-#include <chrono>
-#include <thread>
+#include <unistd.h> // https://man7.org/linux/man-pages/man3/sleep.3.html
 
-std::atomic<int> number{3};
+std::atomic<int> number{3}; // Global Variable
 void *addition(void *_arg)
 {
     int cpuRegister1 = number;
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    // Number is added to the cpu register and there is a delay to when the adding takes place.
+    sleep(1);
     cpuRegister1 += (long)_arg;
     number = cpuRegister1;
-    std::cout << "Added: " << number << "\n";
+    std::string message = "Added: " + std::to_string(number) + "\n";
+    std::cout << message;
 
     char *ret;
     pthread_exit(ret);
@@ -31,7 +31,8 @@ void *subtraction(void *_arg)
     int cpuRegister1 = number;
     cpuRegister1 -= (long)_arg;
     number = cpuRegister1;
-    std::cout << "Subtracted: " << number << "\n";
+    std::string message = "Subtracted: " + std::to_string(number) + "\n";
+    std::cout << message;
 
     char *ret;
     pthread_exit(ret);
@@ -41,7 +42,12 @@ int main()
 {
     pthread_t thread_1, thread_2;
     pthread_create(&thread_1, NULL, addition, (void *)2);
-    pthread_create(&thread_2, NULL, subtraction, (void *)-2);
-    std::cout << "Default: " << number << "\n";
-    return 1;
+    pthread_create(&thread_2, NULL, subtraction, (void *)2);
+
+    std::string message1 = "Main Thread (Before Wait): " + std::to_string(number) + "\n";
+    std::cout << message1;
+    sleep(1);
+    std::string message2 = "Main Thread (After Wait): " + std::to_string(number) + "\n";
+    std::cout << message2;
+    return 0;
 }
