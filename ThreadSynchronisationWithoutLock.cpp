c00c3b55a55d962231e8cf2d66e433f0f1904e5a
Chi Huu Huynh <73843190+Chi+EEE@users.noreply.github.com>
2022-11-16 15:48:05 +0000

    Added comments to Code

--- ThreadSynchronisationWithoutLock.cpp
+++ ThreadSynchronisationWithoutLock.cpp
@@ -4,10 +4,10 @@
 /// Login: C00261172
 /// </summary>
 
-#include <iostream>
-#include <string>
-#include <pthread.h>
-#include <atomic>
+#include <iostream> // std::cout
+#include <string> // std::string
+#include <pthread.h> // pthread
+#include <atomic> // std::atomic
 
 #include <unistd.h> // https://man7.org/linux/man-pages/man3/sleep.3.html
 
@@ -17,31 +17,33 @@ void *addition(void *_arg)
     int cpuRegister1 = number;
     // Number is added to the cpu register and there is a delay to when the adding takes place.
     sleep(1);
-    cpuRegister1 += (long)_arg;
+    cpuRegister1++;
     number = cpuRegister1;
     std::string message = "Added: " + std::to_string(number) + "\n";
     std::cout << message;
 
-    char *ret;
-    pthread_exit(ret);
+    // Without this, vscode sends an warning
+    char *retval;
+    pthread_exit(retval);
 }
 void *subtraction(void *_arg)
 {
     int cpuRegister1 = number;
-    cpuRegister1 -= (long)_arg;
+    cpuRegister1--;
     number = cpuRegister1;
     std::string message = "Subtracted: " + std::to_string(number) + "\n";
     std::cout << message;
 
-    char *ret;
-    pthread_exit(ret);
+    // Without this, vscode sends an warning
+    char *retval;
+    pthread_exit(retval);
 }
 
 int main()
 {
-    pthread_t thread_1, thread_2;
-    pthread_create(&thread_1, NULL, addition, (void *)2);
-    pthread_create(&thread_2, NULL, subtraction, (void *)2);
+    pthread_t thread1, thread2;
+    pthread_create(&thread1, NULL, addition, NULL);
+    pthread_create(&thread2, NULL, subtraction, NULL);
 
     std::string message1 = "Main Thread (Before Wait): " + std::to_string(number) + "\n";
     std::cout << message1;
