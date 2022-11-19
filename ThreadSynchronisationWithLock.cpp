f8db570af47da2678bbe110e2d158abe56359c9f
Chi Huu Huynh <73843190+Chi+EEE@users.noreply.github.com>
2022-11-19 15:45:01 +0000

    Added ThreadSynchronisationWithLock.cpp

--- ThreadSynchronisationWithLock.cpp
+++ ThreadSynchronisationWithLock.cpp
@@ -4,16 +4,18 @@
 /// Login: C00261172
 /// </summary>
 
-#include <iostream> // std::cout
-#include <string> // std::string
+#include <iostream>  // std::cout
+#include <string>    // std::string
 #include <pthread.h> // pthread
-#include <atomic> // std::atomic
+#include <atomic>    // std::atomic
 
 #include <unistd.h> // sleep
 
+pthread_mutex_t mutexLock;
 std::atomic<int> number{3}; // Global Variable
 void *addition(void *_arg)
 {
+    pthread_mutex_lock(&mutexLock);
     int cpuRegister1 = number;
     // Number is added to the cpu register and there is a delay to when the adding takes place.
     sleep(1);
@@ -21,6 +23,7 @@ void *addition(void *_arg)
     number = cpuRegister1;
     std::string message = "Added: " + std::to_string(number) + "\n";
     std::cout << message;
+    pthread_mutex_unlock(&mutexLock);
 
     // Without this, vscode sends an warning
     char *retval;
@@ -28,11 +31,13 @@ void *addition(void *_arg)
 }
 void *subtraction(void *_arg)
 {
+    pthread_mutex_lock(&mutexLock);
     int cpuRegister1 = number;
     cpuRegister1--;
     number = cpuRegister1;
     std::string message = "Subtracted: " + std::to_string(number) + "\n";
     std::cout << message;
+    pthread_mutex_unlock(&mutexLock);
 
     // Without this, vscode sends an warning
     char *retval;
@@ -42,6 +47,7 @@ void *subtraction(void *_arg)
 int main()
 {
     pthread_t thread1, thread2;
+
     pthread_create(&thread1, NULL, addition, NULL);
     pthread_create(&thread2, NULL, subtraction, NULL);
 
@@ -52,5 +58,6 @@ int main()
     sleep(1);
     std::string message2 = "Main Thread (After Wait): " + std::to_string(number) + "\n";
     std::cout << message2;
+    pthread_mutex_destroy(&mutexLock); // Destroy mutex lock
     return 0;
 }
